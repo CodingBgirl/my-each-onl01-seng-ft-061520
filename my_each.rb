0ae3137e8d85527056aef4e3ed9051e29101a38b
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-23 19:34:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,6 @@ def my_each(array)# put argument(s) here
   # array.length
   # counter
   # maybe it start at zero
-  #basic while syntax:
-  #while condition
-  #
-  #end
  counter = 0
  while counter < array.length do
    yield(array[counter])
